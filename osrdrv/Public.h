91e2eb11de7094f4dc54df78fea4411f18ff02f1
Andrew Gaspar <andrew.gaspar@outlook.com>
2016-05-20 01:18:00 -0700

    Initial driver commit. Based on WDK sample KMDF driver with personalizations

---
+++ osrdrv/Public.h
@@ -0,0 +1,24 @@
+/*++
+
+Module Name:
+
+    public.h
+
+Abstract:
+
+    This module contains the common declarations shared by driver
+    and user applications.
+
+Environment:
+
+    user and kernel
+
+--*/
+
+//
+// Define an Interface Guid so that app can find the device and talk to it.
+//
+
+DEFINE_GUID (GUID_DEVINTERFACE_OSR_FX2,
+    0x728c3fef,0x86fd,0x4f28,0x89,0x53,0xcc,0x8a,0xbe,0x62,0x63,0xa2);
+// {728c3fef-86fd-4f28-8953-cc8abe6263a2}
